66511a8d7e9321cefcd088a02702c4da388175cf
isaiahsam <isaiahpascual12@gmail.com>
2024-04-02 16:04:13 +0800

    FINAL FINAL FINAL JUST BEAUTIFY A LITTLE BIT

--- x86-to-C interface programming project/asm1DStencil.asm
+++ x86-to-C interface programming project/asm1DStencil.asm
@@ -1,83 +1,54 @@
-section .data
-output_format db "(ASM) Computed Y: %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, %.4lf, ...", 10, 0
-
 section .text
 bits 64
 default rel
 
 global asm1DStencil
-extern printf
 
-; Performs 1D stencil operation on a double precision floating point array
-; @param rcx - size of the arrays (n)
-; @param rdx - pointer to the input array (X)
-; @param r8 - pointer to the output array (Y)
 asm1DStencil:
-    sub rsp, 128          ; Reserve stack space for local variables and alignment
-    
-    mov rsi, rdx          ; RSI points to the start of X
-    mov rdi, r8           ; RDI points to the start of Y
-    xor rdx, rdx          ; RDX will keep count of processed elements for Y
-    
-    ; Zeroing out registers used for sums to ensure clean state
-    xorpd xmm0, xmm0
-    xor r9, r9            ; Counter for the main loop
+    sub rsp, 8*11
 
-MainLoop:
-    cmp r9, rcx
-    jge Finish            ; Jump to finish when all elements processed
-    
-    ; Reset xmm0 for new sum
-    xorpd xmm0, xmm0
-    
-    ; Load and sum 7 consecutive elements from X into xmm0
-    movsd xmm1, [rsi + r9*8 - 24] ; X[i-3]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8 - 16] ; X[i-2]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8 - 8]  ; X[i-1]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8]      ; X[i]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8 + 8]  ; X[i+1]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8 + 16] ; X[i+2]
-    addsd xmm0, xmm1
-    movsd xmm1, [rsi + r9*8 + 24] ; X[i+3]
-    addsd xmm0, xmm1
-    
-    ; Store the sum in Y
-    movsd [rdi + r9*8], xmm0
-    
-    inc r9
-    jmp MainLoop
+    ; rsi - length, rdx - inputArray, r8 - outputArray
+    mov rsi, rcx    ; Length of the array
+    mov rdx, rdx    ; Address of inputArray
+    mov rdi, r8     ; Address of outputArray
 
-Finish:
-    ; Determine the number of results to print (minimum of r9 and 10)
-    cmp r9, 10
-    jle PreparePrint
-    mov r9, 10
+    ; Index initialization
+    xor rax, rax    ; Zero out rax to use as our index
 
-PreparePrint:
-    lea rcx, [output_format] ; Message format
-    ; Assuming we want to print the first 10 or fewer elements
-    ; Load up to 10 elements from Y into registers for printf
-    ; We start from the last element to print and move backwards
-    mov rax, rdi
-    add rax, 8*9          ; Adjust rax to point to the 10th element or last
-    movq xmm1, [rax-72]  ; Y[0] or earlier
-    movq xmm2, [rax-64]
-    movq xmm3, [rax-56]
-    movq xmm4, [rax-48]
-    movq xmm5, [rax-40]
-    movq xmm6, [rax-32]
-    movq xmm7, [rax-24]
-    movq xmm8, [rax-16]
-    movq xmm9, [rax-8]
-    movq xmm0, [rax]     ; Y[9] or last available
-    sub rsp, 8*4         ; Shadow space for Windows ABI
-    call printf          ; Print the elements
-    add rsp, 8*4         ; Clean up shadow space
+LOOP:
+    cmp rax, rsi
+    jge end         ; Jump to end if rax >= length (rsi)
 
-    add rsp, 128         ; Restore stack space
-    ret                  ; Return to caller
+    ; Skip the first 3 and last 3 elements by starting from index 3 and ending at length - 3
+    cmp rax, 3
+    jl SKIP_INCREMENT
+    cmp rax, rsi
+    jge SKIP_INCREMENT
+
+    ; Stencil computation
+    xorpd xmm0, xmm0           ; Clear xmm0
+    movsd xmm1, [rdx+rax*8-24] ; Load inputArray[index-3]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8-16] ; Load inputArray[index-2]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8-8]  ; Load inputArray[index-1]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8]    ; Load inputArray[index]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8+8]  ; Load inputArray[index+1]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8+16] ; Load inputArray[index+2]
+    addsd xmm0, xmm1
+    movsd xmm1, [rdx+rax*8+24] ; Load inputArray[index+3]
+    addsd xmm0, xmm1
+
+    ; Store the result into the output array
+    movsd [rdi+rax*8], xmm0
+
+SKIP_INCREMENT:
+    inc rax        ; Increment our index
+    jmp SHORT LOOP ; Jump back to the start of the loop
+
+end:
+    add rsp, 8*11
+    ret
--- x86-to-C interface programming project/stencil.c
+++ x86-to-C interface programming project/stencil.c
@@ -18,26 +18,21 @@ double calculate_duration(clock_t startTime, clock_t endTime) {
 }
 
 int main() {
-    int counter;
-    clock_t startCompute, endCompute;
-    double computationTime;
-
     int dataSizes[] = { 1 << 20, 1 << 24, 1 << 28 }; // {2^20, 2^24, 2^28}
     int totalSizes = sizeof(dataSizes) / sizeof(dataSizes[0]);
     int exponentSizes[] = { 20, 24, 28 };
-
     double* inputData = (double*)malloc(dataSizes[totalSizes - 1] * sizeof(double));
     if (!inputData) {
         fprintf(stderr, "Failed to allocate memory\n");
         return EXIT_FAILURE;
     }
 
-    for (counter = 0; counter < dataSizes[totalSizes - 1]; counter++) {
+    for (int counter = 0; counter < dataSizes[totalSizes - 1]; counter++) {
         inputData[counter] = counter + 1;
     }
 
     printf("\n===== Initial Values in inputData =====\n");
-    for (counter = 0; counter < FIRST_TEN; counter++) {
+    for (int counter = 0; counter < FIRST_TEN; counter++) {
         printf(" inputData[%d] = %.2lf\n", counter, inputData[counter]);
     }
 
@@ -45,43 +40,46 @@ int main() {
 
     for (int idx = 0; idx < totalSizes; idx++) {
         int currentSize = dataSizes[idx];
-        double* resultArray = (double*)malloc(currentSize * sizeof(double));
+        double* resultArrayC = (double*)malloc(currentSize * sizeof(double));
+        double* resultArrayASM = (double*)malloc(currentSize * sizeof(double));
+        if (!resultArrayC || !resultArrayASM) {
+            fprintf(stderr, "Failed to allocate memory for result arrays\n");
+            free(resultArrayC);
+            free(resultArrayASM);
+            continue; // Skip this iteration
+        }
 
         printf("\n--> Analyzing Performance for Array Size 2^%d <--\n", exponentSizes[idx]);
 
-        for (counter = 0; counter < currentSize; counter++) {
-            resultArray[counter] = 0.0;
-        }
+        // C Version
+        clock_t startComputeC = clock();
+        compute_stencil(currentSize, inputData, resultArrayC);
+        clock_t endComputeC = clock();
+        double computationTimeC = calculate_duration(startComputeC, endComputeC);
 
-        startCompute = clock();
-        compute_stencil(currentSize, inputData, resultArray);
-        endCompute = clock();
-        computationTime = calculate_duration(startCompute, endCompute);
-        printf("\nComputation Time (C Function) for Array Size %d: %lf seconds\n", currentSize, computationTime);
+        // Assembly Version
+        clock_t startComputeASM = clock();
+        asm1DStencil(currentSize, inputData, resultArrayASM);
+        clock_t endComputeASM = clock();
+        double computationTimeASM = calculate_duration(startComputeASM, endComputeASM);
 
+        // Print C Function Output
         printf("\n--- First Ten Calculated Values in resultArray (C Function) ---\n");
-        for (counter = 3; counter < FIRST_TEN + 3 && counter < currentSize - 3; counter++) {
-            printf(" resultArray[%d] = %.2lf\n", counter, resultArray[counter]);
+        for (int counter = 3; counter < FIRST_TEN + 3 && counter < currentSize - 3; counter++) {
+            printf(" resultArrayC[%d] = %.2lf\n", counter, resultArrayC[counter]);
         }
+        printf("\nComputation Time (C Function) for Array Size %d: %lf seconds\n", currentSize, computationTimeC);
 
-        // Call to asm1DStencil would go here, using the same pattern as above for timing and displaying values
-        // Since we can't actually execute assembly, this part is commented out
-        /*
-        start_c = clock();
-        asm1DStencil(n, X, Y);
-        end_c = clock();
-        cpu_time_used_c = get_execution_time(start_c, end_c);
-        printf("\nTime taken by assembly implementation for vector length %d: %lf seconds", n, cpu_time_used_c);
-
-        printf("\nFirst ten calculated values of vector Y (assembly implementation):\n");
-        for (i = 3; i < N + 3 && i < n - 3; i++) {
-            printf("%.2lf ", Y[i]);
+        // Print Assembly Function Output
+        printf("\n--- First Ten Calculated Values in resultArray (Assembly Function) ---\n");
+        for (int counter = 3; counter < FIRST_TEN + 3 && counter < currentSize - 3; counter++) {
+            printf(" resultArrayASM[%d] = %.2lf\n", counter, resultArrayASM[counter]);
         }
-        */
-
+        printf("\nComputation Time (Assembly Function) for Array Size %d: %lf seconds\n", currentSize, computationTimeASM);
         printf("\n=================================================================\n");
 
-        free(resultArray);
+        free(resultArrayC);
+        free(resultArrayASM);
     }
 
     free(inputData);
